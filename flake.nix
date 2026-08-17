3a5697adad9fa4ae9e7a6f8901801cf4cd5ac67b
Fovir <fovir@disroot.org>
2026-08-17 15:31:56 +0800

    style: format code
    Signed-off-by: Fovir <fovir@disroot.org>

--- flake.nix
+++ flake.nix
@@ -3,29 +3,48 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-    forEachSupportedSystem = f:
-      nixpkgs.lib.genAttrs supportedSystems (system:
-        f {
-          pkgs = import nixpkgs {inherit system;};
-        });
-  in {
-    overlays.default = import ./nix/overlay.nix;
-    nixosModules.default = import ./nix/options.nix;
-    packages = forEachSupportedSystem ({pkgs}: {
-      vexgo = pkgs.callPackage ./nix/package.nix {};
-      default = pkgs.callPackage ./nix/package.nix {};
-    });
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      overlays.default = import ./nix/overlay.nix;
+      nixosModules.default = import ./nix/options.nix;
+      packages = forEachSupportedSystem (
+        { pkgs }: {
+          vexgo = pkgs.callPackage ./nix/package.nix { };
+          default = pkgs.callPackage ./nix/package.nix { };
+        }
+      );
 
-    devShells = forEachSupportedSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [pnpm nodejs go];
-      };
-    });
-  };
+      devShells = forEachSupportedSystem (
+        { pkgs }: {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              go
+              nodejs
+              pnpm
+            ];
+          };
+        }
+      );
+    };
 }
